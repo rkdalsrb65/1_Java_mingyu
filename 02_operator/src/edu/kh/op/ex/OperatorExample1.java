2fb34c674ae70f19191f9f0d331d89a5e168ec30
rkdalsrb65 <rkdalsrb65@naver.com>
2022-08-08 10:54:57 +0900

    연산자
    연산자

---
+++ 02_operator/src/edu/kh/op/ex/OperatorExample1.java
@@ -0,0 +1,23 @@
+package edu.kh.op.ex;
+
+import java.util.Scanner;
+
+public class OperatorExample1 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner
+		
+		
+		// 5 / 3 = 1 (몫)
+		System.out.println("%d / %d = %d \n");
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
