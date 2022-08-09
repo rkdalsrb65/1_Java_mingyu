f2c01f45d326b8b525ea9c7ea564106c9cca75bc
rkdalsrb65 <rkdalsrb65@naver.com>
2022-08-09 12:47:38 +0900

    Update SwitchExample.java

--- 03_control/src/edu/kh/control/condition/SwitchExample.java
+++ 03_control/src/edu/kh/control/condition/SwitchExample.java
@@ -221,29 +221,23 @@ public class SwitchExample {
 		
 		String season;
 		
+		// switch문에서 break는
+		// "해당 case를 수행한 후 멈춰라" 라는 뜻
+		
+		// break가 없을 경우
+		// -> 다음 case가 연달아서 수행된다!
+				
 		switch(month) {
-		case 3 : season = "봄"; break;
-		case 4 : season = "봄"; break;
-		case 5 : season = "봄"; break;
-		
-		case 6 : season = "여름"; break;
-		case 7 : season = "여름"; break;
-		case 8 : season = "여름"; break;
-		
-		case 9 : season = "가을"; break;
-		case 10 : season = "가을"; break;
-		case 11 : season = "가을"; break;
-		
-		case 12 : season = "겨울"; break;
-		case 1 : season = "겨울"; break;
-		case 2 : season = "겨울"; break;
+		case 3 : case 4 : case 5 : season = "봄"; break;		
+		case 6 : case 7 : case 8 : season = "여름"; break;		
+		case 9 : case 10 : case 11 : season = "가을"; break;		
+		case 12 : case 1 : case 2 : season = "겨울"; break;
 		
 		default : season = "잘못 입력 하셨습니다.";
 		}
 		
 		System.out.println(season);
-		
-		
+				
 	}
 	
 }
