21435add00d385c40c72d38ca480aa4b9fbe1f23
rkdalsrb65 <rkdalsrb65@naver.com>
2022-08-10 09:56:09 +0900

    반복문

---
+++ 03_control/src/edu/kh/control/loop/LoopRun.java
@@ -0,0 +1,20 @@
+package edu.kh.control.loop;
+
+public class LoopRun {
+
+	public static void main(String[] args) {
+		
+/*		 초기식 : for문을 제어하는 용도의 변수 선언
+  
+		 조건식 : for문의 반복 여부를 지정하는 식
+		        조건식이 true인 경우에 반복을 수행함
+		        보통 초기식에 사용된 변수를 이용해서 조건식을 작성함.
+		        
+		 증감식 : for문이 끝날 때 마다 특정 값을 변화 시키는 식
+		        보통 초기식에 사용된 변수를 증가/감소 시켜 조건식의 결과를 변하게 만듦.		*/
+		
+		ForExample forEx = new ForExample();
+
+	}
+
+}
