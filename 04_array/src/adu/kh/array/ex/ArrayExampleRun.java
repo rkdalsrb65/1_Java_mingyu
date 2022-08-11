c8bf0b97ae763df9dcf45e13ceebff9eae55ab46
rkdalsrb65 <rkdalsrb65@naver.com>
2022-08-11 17:07:50 +0900

    배열

---
+++ 04_array/src/adu/kh/array/ex/ArrayExampleRun.java
@@ -0,0 +1,16 @@
+package adu.kh.array.ex;
+
+public class ArrayExampleRun {
+
+	public static void main(String[] args) {
+		
+		ArrayExample arrayEx = new ArrayExample();
+		
+		arrayEx.ex1();
+		
+		
+		
+
+	}
+
+}
