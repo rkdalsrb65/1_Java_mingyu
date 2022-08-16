f9c8592dc9d2d80f24a3cfe858c18a9c25413a43
rkdalsrb65 <rkdalsrb65@naver.com>
2022-08-16 22:51:30 +0900

    Update test2.java

--- 05_test/src/test2.java
+++ 05_test/src/test2.java
@@ -310,7 +310,7 @@ BMI 지수 : 21.45087235996327
 		
 
 
-//********** 조건문 실습문제5 **********
+//********** 조건문 실습문제5 **********                      보류
 //메소드 명 : public void practice5(){}
 //중간고사, 기말고사, 과제점수, 출석횟수를 입력하고 Pass 또는 Fail을 출력하세요.
 //평가 비율은 중간고사 20%, 기말고사 30%, 과제 30%, 출석 20%로 이루어져 있고
@@ -351,54 +351,47 @@ BMI 지수 : 21.45087235996327
 //================= 결과 =================
 //Fail [출석 횟수 부족 (10/20)]		
 		
-		Scanner sc = new Scanner(System.in);
-		
-		System.out.print("중간 고사 점수 : ");
-		double wndrks = sc.nextDouble();
-		
-		System.out.print("기말 고사 점수 : ");
-		double rlakf = sc.nextDouble();
-		
-		System.out.print("과제 점수 : ");
-		double rhkwp = sc.nextDouble();
-		
-		System.out.print("출석 점수 : ");
-		double cnftjr = sc.nextDouble();
-		
-		wndrks = wndrks*0.2;       // midTerm    *= 0.2;
-		rlakf = rlakf*0.3;   // finalTerm  *= 0.3;
-		rhkwp = rhkwp*0.3;         // report     *= 0.3;
-		cnftjr = (cnftjr*5)*0.2; // attendance *= 5 * 0.2;
-		
-		
-		double sum = wndrks + rlakf + rhkwp + cnftjr;
-		
-		if(cnftjr <= 20 * (1 - 0.3)) {
-			System.out.println("Fail [출석 횟수 부족 (" + cnftjr + "/20)");
-		} else if(sum < 70.0) {
-			System.out.println("중간 고사 점수 : ");
-			System.out.println("기말 고사 점수 : ");
-			System.out.println("과제 점수 : ");
-			System.out.println("출석 횟수 : ");
-			System.out.println("총점 : " + sum);
-			System.out.println("Fail [점수 미달]");
-		} else {
-			
-			System.out.println("PASS");
-		}
-		
-		
+//		Scanner sc = new Scanner(System.in);
+//		
+//		System.out.print("중간 고사 점수 : ");
+//		double wndrks = sc.nextDouble();
+//		
+//		System.out.print("기말 고사 점수 : ");
+//		double rlakf = sc.nextDouble();
+//		
+//		System.out.print("과제 점수 : ");
+//		double rhkwp = sc.nextDouble();
+//		
+//		System.out.print("출석 점수 : ");
+//		double cnftjr = sc.nextDouble();
+//		
+//		wndrks = wndrks*0.2;       // midTerm    *= 0.2;
+//		rlakf = rlakf*0.3;   // finalTerm  *= 0.3;
+//		rhkwp = rhkwp*0.3;         // report     *= 0.3;
+//		cnftjr = (cnftjr*5)*0.2; // attendance *= 5 * 0.2;
+//		
+//		
+//		double sum = wndrks + rlakf + rhkwp + cnftjr;
+//		
+//		if(cnftjr <= 20 * (1 - 0.3)) {
+//			System.out.println("Fail [출석 횟수 부족 (" + cnftjr + "/20)");
+//		} else if(sum < 70.0) {
+//			System.out.println("중간 고사 점수 : " + wndrks);
+//			System.out.println("기말 고사 점수 : " + rlakf);
+//			System.out.println("과제 점수 : " + rhkwp);
+//			System.out.println("출석 횟수 : " + cnftjr);
+//			System.out.println("총점 : " + sum);
+//			System.out.println("Fail [점수 미달]");
+//		} else {
+//			
+//			System.out.println("PASS");
+//		}
 		
 		
 
 
-
-
-
-
-
 		
-/* ********* 반복문 실습문제1 **********
+/* ********* 반복문 실습문제1 **********                    통과
 사용자로부터 한 개의 값을 입력 받아 1부터 그 숫자까지의 숫자들을 모두 출력하세요.
 단, 입력한 수는 1보다 크거나 같아야 합니다.
 만일 1 미만의 숫자가 입력됐다면 “1 이상의 숫자를 입력해주세요“를 출력하세요.
@@ -406,12 +399,19 @@ ex.
 1이상의 숫자를 입력하세요 : 4        1이상의 숫자를 입력하세요 : 0
 1 2 3 4                       1 이상의 숫자를 입력해주세요. */
 
+//Scanner sc = new Scanner(System.in);
+//
+//System.out.print("1이상의 숫자를 입력하세요 : ");
+//int input = sc.nextInt();
+//
+//if(input <= 0) {
+//	System.out.println("1 이상의 숫자를 입력하세요");
+//}for(int i = 1; i <= input; i++) {
+//	System.out.print(i + " ");
+//}
 
 
-
-
-
-/* ********* 반복문 실습문제2 **********		
+/* ********* 반복문 실습문제2 **********		                통과
 사용자로부터 한 개의 값을 입력 받아 1부터 그 숫자까지의 모든 숫자를 거꾸로 출력하세요.
 단, 입력한 수는 1보다 크거나 같아야 합니다.
 ex.
@@ -419,19 +419,45 @@ ex.
 4 3 2 1                        1 이상의 숫자를 입력해주세요. */
 
 
+//		Scanner sc = new Scanner(System.in);
+//		
+//		System.out.print("1이상의 숫자를 입력하세요 : ");
+//		int input = sc.nextInt();
+//		
+//		if (input >= 1) {
+//			for (int i = input; i >= 1; i--) {
+//				System.out.print(i + " ");
+//			}
+//		} else {
+//			System.out.println("1 이상의 숫자를 입력해주세요.");
+//		}
+		
+		
 
-/* ********* 반복문 실습문제3 **********		
+/* ********* 반복문 실습문제3 **********		             통과
 1부터 사용자에게 입력 받은 수까지의 정수들의 합을 for문을 이용하여 출력하세요.
 
 ex.
 정수를 하나 입력하세요 : 8
 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 = 36      */
 
-
-
-
-
-
+//		Scanner sc = new Scanner(System.in);
+//		
+//		System.out.print("정수를 하나 입력하세요 : ");
+//		int input = sc.nextInt();
+//		
+//		int sum = 0;
+//		
+//		for(int i=1; i <= input; i++) {
+//			sum += i;
+//			
+//			if(i < input) {	
+//					System.out.print(i + " + ");
+//		} else {
+//			System.out.println(i + " = " + sum);
+//		}
+//	}
+		
 /* ********* 반복문 실습문제4 **********		
 사용자로부터 두 개의 값을 입력 받아 그 사이의 숫자를 모두 출력하세요.
 만일 1 미만의 숫자가 입력됐다면 “1 이상의 숫자를 입력해주세요“를 출력하세요.
@@ -442,7 +468,57 @@ ex.
 4 5 6 7 8        4 5 6 7 8       1 이상의 숫자를 입력해주세요. */
 
 
+Scanner sc = new Scanner(System.in);
+		
+		System.out.print("첫 번째 숫자 : ");
+		int num1 = sc.nextInt();
+		
+		System.out.print("두 번째 숫자 : ");
+		int num2 = sc.nextInt();
 
+		if( num1 < 1  ||  num2 < 1 ) { // num1, num2 중에 1 미만이 있는가?
+			System.out.println("1 이상의 숫자를 입력해주세요.");
+			
+		} else { // 둘 다 1 미만이 아니다
+			//  8       4
+			if(num1 > num2) { 
+				
+				// 두 변수의 값 교환
+				int temp = num1;  //  temp : 8 / num1 : 8 / num2 : 4
+				num1 = num2; //  temp : 8 / num1 : 4 / num2 : 4
+				num2 = temp; //  temp : 8 / num1 : 4 / num2 : 8
+			}
+			
+			for(int i=num1 ; i<=num2 ; i++) {
+				System.out.print(i + " ");
+			}
+			
+			
+			/*int start = num1;
+			int end = num2;
+			
+			if(num1 > num2) { // 먼저 입력한 숫자가 크면 start / end를 바꿈
+				start = num2;
+				end = num1;
+			}
+			
+			for(int i = start ; i <= end ; i++) {
+				System.out.print(i + " ");
+			}*/
+			
+			
+			
+			/*if(num1 < num2) {
+				for(int i=num1 ; i<=num2 ; i++) {
+					System.out.print(i + " ");
+				}
+			} else {
+				for(int i=num2 ; i<=num1 ; i++) {
+					System.out.print(i + " ");
+				}
+			}*/
+			
+		}
 
 
 
@@ -461,6 +537,17 @@ ex.
 4 * 7 = 28
 4 * 8 = 32
 4 * 9 = 36     */
+		
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("숫자 : ");
+		int dan = sc.nextInt();		
+		
+		System.out.printf("===== %d단 =====\n", dan);
+		for(int i = 1; i < 10; i++) {			
+			System.out.printf("%d * %d = %d%n", dan, i, (dan * i));
+		}		
+		
 /* ********* 반복문 실습문제6 **********		
 사용자로부터 입력 받은 숫자의 단부터 9단까지 출력하세요.
 단, 2~9를 사이가 아닌 수를 입력하면 “2~9 사이 숫자만 입력해주세요”를 출력하세요.
@@ -474,7 +561,31 @@ ex.
 ===== 9단 =====
 (해당 단의 내용들은 길이 상 생략) */
 
-
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("숫자 : ");
+		int num = sc.nextInt();
+		
+		if( num >= 2 && num <=9 ) {
+			
+			for(int dan = num ; dan <= 9 ; dan++) {
+				
+				System.out.println("===== " + dan + "단 =====");
+				
+				for(int i = 1 ; i<=9 ; i++) {
+					System.out.printf("%d X %d = %2d \n" , dan , i , dan * i);
+				}
+				
+				System.out.println(); // 단 사이 줄 바꿈
+			}
+			
+		} else {
+			System.out.println("2~9 사이 숫자만 입력해주세요");
+		}
+		
+		
+		
+		
 /* ********* 반복문 실습문제7 **********		
 다음과 같은 실행 예제를 구현하세요.
 
@@ -485,7 +596,17 @@ ex.
 ***
 ****                              */
 
+		Scanner sc = new Scanner(System.in);
 
+		System.out.print("정수 입력 : ");
+		int input = sc.nextInt();			
+		
+		for(int row = 1; row <= input; row++) {
+			for(int col = 1; col <= row; col++) {
+				System.out.print("*");
+			}
+			System.out.println(); // 줄 바꿈 (개행)
+		}
 
 
 
@@ -499,6 +620,18 @@ ex.
 **
 *                                */
 
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("정수 입력 : ");
+		int input = sc.nextInt();			
+		
+		for(int x = input; x > 0; x--) {
+			for(int i = x; i > 0; i--) {
+				System.out.print("*");
+			}
+			System.out.println(); // 줄 바꿈 (개행)
+		}	
+		
 
 /* ********* 반복문 실습문제9 **********		
 다음과 같은 실행 예제를 구현하세요.
@@ -510,7 +643,41 @@ ex.
 ***
 ****                               */
 
-
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("정수 입력 : ");
+		int input = sc.nextInt();
+		
+		for(int row = 1 ; row <= input ; row++ ) {
+			
+			// for문을 이용한 풀이
+			//for(int i = input-row ; i >= 1 ; i--) {
+//			for(int i = input-1 ; i >= row ; i--) {
+//				System.out.print(" ");
+//			}
+			
+//			for(int col = 1 ; col <= row ; col++) {
+//				System.out.print("*");
+//			}
+			
+			
+			// if문을 이용한 풀이
+			for(int col = 1 ; col <= input ; col++) {
+				
+				if(col <= input - row) {
+					System.out.print(" ");
+					
+				}else {
+					System.out.print("*");
+				}
+				
+			}
+			
+			
+			System.out.println(); // 줄바꿈
+		}
+		
+		
 /* ********* 반복문 실습문제10 **********		
 다음과 같은 실행 예제를 구현하세요.
 
@@ -521,7 +688,44 @@ ex.
 ***
 **
 *                                  */
+		Scanner sc = new Scanner(System.in);
 
+		System.out.print("정수 입력 : ");
+		int input = sc.nextInt();
+		
+		// 위쪽 삼각형
+		for(int row = 1; row <= input; row++) {
+			
+			for(int col = 1; col <= row; col++) {
+				System.out.print("*");
+			}
+			System.out.println(); // 줄바꿈
+		}
+		
+		// 아랫쪽 삼각형
+		for(int row = input-1; row >= 1; row--) {
+			
+			for(int col = 1; col <= row; col++) {
+				System.out.print("*");
+			}
+			System.out.println(); // 줄바꿈
+		}
+		
+		System.out.println("=================");
+		
+		for(int row = 1; row <= input*2-1; row++) {
+			if(row<input) {
+				
+				for(int col=1; col <= row; col++) {
+					System.out.print("*");
+				}
+			} else {
+				for(int col=input; col > row-input; col--) {
+					System.out.print("*");
+				}
+			}
+			System.out.println();
+		}
 
 /* ********* 반복문 실습문제11 **********		
 다음과 같은 실행 예제를 구현하세요.
@@ -532,7 +736,38 @@ ex.
 ***
 *****                              */
 
+		Scanner sc = new Scanner(System.in);
 
+		System.out.print("정수 입력 : ");
+		int input = sc.nextInt();
+		
+		for(int row = 1 ; row <= input ; row++ ) {
+			
+			// for문을 이용한 풀이
+//			for(int i = input-row ; i >= 1 ; i--) {
+/*			for(int i = input-1 ; i >= row ; i--) {
+				System.out.print(" ");
+			}
+			
+			for(int col = 1 ; col <= row * 2-1; col++) {
+				System.out.print("*");
+			}                                            */
+			
+			// if문을 이용한 풀이
+			for(int col = 1 ; col <= input * 2-1 ; col++) {
+				
+				if(input-row>=col||input+row<=col) {
+					System.out.print(" ");
+					
+				}else {
+					System.out.print("*");
+				}
+				
+			}
+			
+			
+			System.out.println(); // 줄바꿈
+		}
 
 /* ********* 반복문 실습문제12 **********		
 다음과 같은 실행 예제를 구현하세요.
@@ -544,7 +779,25 @@ ex.
 * *
 * *
 *****                             */
+		
+		Scanner sc = new Scanner(System.in);
 
+		System.out.print("정수 입력 : ");
+		int input = sc.nextInt();				
+		
+		for(int row = 1; row <= input; row++) {
+			for(int col = 1; col <= input; col++) {
+				
+				// 첫 번째 / 마지막 줄, 칸일 때만 * 출력
+				if(row == 1 || row == input || col == 1 || col == input) {
+					System.out.print("*");
+				} else {
+				System.out.print(" ");
+			}
+		}
+			System.out.println();
+		
+	}
 
 
 /* ********* 반복문 실습문제13 **********		
@@ -560,7 +813,29 @@ ex.
 2 3 4 6 8 9 10 12 14 15
 count : 2                           */		
 		
+		Scanner sc = new Scanner(System.in);
 		
+		System.out.print("자연수 하나를 입력하세요 : ");
+		int input = sc.nextInt();	
+		
+		int sum = 0;
+		int count = 0;
+		
+		
+		for(int i = 1; i <= input; i++) {
+			
+			if(i % 2 == 0 || i % 3 == 0) { // 2의 배수 또는 3의 배수인 경우 출력
+				System.out.print(i + " ");
+				
+				// 2와 3의 공배수 == 2로도 나누어 떨어지고, 3으로도 나누어 떨어진다
+				if(i % 2 == 0 && i % 3 == 0) {
+				count++; // 1증가		
+				}
+		}
+		
+			
+	}
+		System.out.println("\ncount : " + count);		
 		
 		
 		
