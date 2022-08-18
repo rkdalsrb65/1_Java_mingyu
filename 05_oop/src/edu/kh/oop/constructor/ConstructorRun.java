ddb169ffb94dec629d7b84bdc22f942d377b350e
rkdalsrb65 <rkdalsrb65@naver.com>
2022-08-18 17:31:13 +0900

    생성자

--- 05_oop/src/edu/kh/oop/constructor/ConstructorRun.java
+++ 05_oop/src/edu/kh/oop/constructor/ConstructorRun.java
@@ -7,13 +7,27 @@ public class ConstructorRun {
 	public static void main(String[] args) {
 		
 		Member member1 = new Member();
-		                 // 생성자
+		                 // 기본 생성자
+		
+		Member member2 = new Member();
+		Member member3 = new Member();
+		Member member4 = new Member();
+		                 // 기본 생성자
+		
+		// 기본 생성자가 없어도 Member() 구문에서 에러가 발생하지 않는다!
+		// -> 클래스에 생성자가 하나도 작성되지 않으면
+		//    컴파일러가 자동으로 기본생성자를 추가해준다!
+		//    public Member(){} // Member 클래스에 자동 추가
+		
+		
+		Member member5 = new Member("member22", "pass22@", "010-2222-3333", 33);
+		                         // -> 전달 인자, 인자(Argument)
+		
+		Member member6 = new Member("아이디", "비밀번호", "010-1-1", 1);
+		Member member7 = new Member("아디", "비번", "010-2-2", 2);
+		
 		
 		System.out.println("프로그램 종료");
-		
-		
-		
-		
 
 	}
 
