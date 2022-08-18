a4944c256ed7d9dea812900e26bc064078dd30f8
rkdalsrb65 <rkdalsrb65@naver.com>
2022-08-18 12:30:20 +0900

    객체

--- 05_oop/src/edu/kh/oop/basic/KMG.java
+++ 05_oop/src/edu/kh/oop/basic/KMG.java
@@ -1,6 +1,6 @@
-package eud.kh.oop.basic;
+package edu.kh.oop.basic;
 
-// 클래스 : 객체가 되었을 때 가지고 있을 속성, 기능을 정의(작성)한 문서
+// 클래스 : 객체가 되었을 때 가지고 있을 속성, 기능을 정의(작성)한 문서(설계도 같은)
 public class KMG {
 
 	// 객체가 가지고 있는 속성
