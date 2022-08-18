71718e8a799b0f2e8e60f4fe0dfbe24408e73cfb
rkdalsrb65 <rkdalsrb65@naver.com>
2022-08-18 15:01:11 +0900

    객체

---
+++ 05_oop/src/edu/kh/oop/constructor/ConstructorRun.java
@@ -0,0 +1,20 @@
+package edu.kh.oop.constructor;
+
+import edu.kh.oop.constructor.model.vo.Member;
+
+public class ConstructorRun {
+
+	public static void main(String[] args) {
+		
+		Member member1 = new Member();
+		                 // 생성자
+		
+		System.out.println("프로그램 종료");
+		
+		
+		
+		
+
+	}
+
+}
---
+++ 05_oop/src/edu/kh/oop/constructor/model/vo/Member.java
@@ -0,0 +1,20 @@
+package edu.kh.oop.constructor.model.vo;
+
+public class Member {
+
+	// 추상화 -> 아이디, 비밀번호, 전화번호, 나이
+	
+	// 캡슐화 -> 추상화 결과를 묶고, 데이터 직접 접근 제한
+	private String memberId;
+	private String memberPw;
+	private String memberPhone;
+	private int memberAge;
+	
+	
+	
+	
+	
+	
+	
+	
+}
