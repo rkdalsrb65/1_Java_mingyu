7de8933f5df9f1db95de436b51dc2f7c82614a81
rkdalsrb65 <rkdalsrb65@naver.com>
2022-08-25 10:19:51 +0900

    컬렉션

--- 10_collection/src/edu/kh/collection/model/service/ListService.java
+++ 10_collection/src/edu/kh/collection/model/service/ListService.java
@@ -22,6 +22,7 @@ public class ListService {
 		// 1) 크기 제약이 없다
 		// 2) 추가, 삭제, 수정, 정렬 등의 기능이 구현되어 있다.
 		// 3) 여러 타입의 객체를 저장할 수 있다.
+		//     -> Object 참조 변수의 묶음
 		
 		// java.util 패키지
 //		ArrayList list = new ArrayList(); // 기본 생성자 -> 10칸짜리 생성
@@ -32,6 +33,20 @@ public class ListService {
 		list.add(new Object());
 		list.add(new Student());
 		list.add(new String());
+		
+		// ArrayList의 크기 3을 초과에서 추가
+		list.add(new String("초과")); // 크기가 자동으로 변함을 확인
+		
+		// add(int index, E e) : index 위치에서 E 객체를 추가
+		list.add(1, new Student("홍길동", 15, "강남구", '남', 60));
+		
+		// set(int index, E e) : indx 위치를 E 객체로 수정
+		// 원래 위치하던 객체를 반환
+		Object str = list.set(4, new String("수정된 문자열"));
+		
+		// remove(int index) : index 위치의 객체를 꺼내서 반환
+		Object student = list.remove(2);
+		
 		
 		System.out.println("종료");
 		
