f045d0d138a28e26356930682c6ad91e24d67736
rkdalsrb65 <rkdalsrb65@naver.com>
2022-08-26 16:43:56 +0900

    네트워크

--- 12_network/src/edu/kh/network/server/run/ServerRun.java
+++ 12_network/src/edu/kh/network/server/run/ServerRun.java
@@ -3,13 +3,10 @@ package edu.kh.network.server.run;
 import edu.kh.network.server.model.service.ServerService;
 
 public class ServerRun {
-
 	public static void main(String[] args) {
 		
 		ServerService service = new ServerService();
 		
 		service.serverStart();
-
 	}
-
 }
