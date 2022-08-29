fb5cee16ef489c7474c179fdf7da0bafb2ff58a8
rkdalsrb65 <rkdalsrb65@naver.com>
2022-08-29 10:42:34 +0900

    네트워크

--- 12_network/src/edu/kh/network/client/run/ClientRun.java
+++ 12_network/src/edu/kh/network/client/run/ClientRun.java
@@ -6,6 +6,8 @@ public class ClientRun {
 	public static void main(String[] args) {
 		
 		ClientService service = new ClientService();
+//		ClientService 라는 객체를 참조변수 service로 선언 후 new (heap영역에 저장)
+		
 		
 		service.clientStart();
 	}
--- 12_network/src/edu/kh/network/server/run/ServerRun.java
+++ 12_network/src/edu/kh/network/server/run/ServerRun.java
@@ -6,6 +6,8 @@ public class ServerRun {
 	public static void main(String[] args) {
 		
 		ServerService service = new ServerService();
+//		ServerService 라는 객체를 참조변수 service로 선언 후 new (heap영역에 저장)
+		
 		
 		service.serverStart();
 	}
