d05c32da5fea3d4179586a9f2ecd29411a670de2
rkdalsrb65 <rkdalsrb65@naver.com>
2022-09-01 21:19:50 +0900

    사원 관리 프로그램 Run

--- EmployeeManagement/src/edu/kh/emp/run/EmployeeRun.java
+++ EmployeeManagement/src/edu/kh/emp/run/EmployeeRun.java
@@ -3,10 +3,8 @@ package edu.kh.emp.run;
 import edu.kh.emp.view.EmployeeView;
 
 public class EmployeeRun {
-
 	public static void main(String[] args) {
 		
 		new EmployeeView().displayMenu();
 	}
-
 }
